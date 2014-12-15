a497519ec2a035d1ae2e72b39ae3ca0d9c729a44
Jérôme Macias <jerome.macias@gmail.com>
2014-12-16 00:19:38 +0100

    Fix and add terminate to set Content-Type and Accept header

--- src/Stack/JsonRequest.php
+++ src/Stack/JsonRequest.php
@@ -3,25 +3,38 @@
 namespace Stack;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\TerminableInterface;
 
-class JsonRequest implements HttpKernelInterface
+class JsonRequest implements HttpKernelInterface, TerminableInterface
 {
     private $app;
-    private $contentTypes = array('application/json');
+    private $contentType;
 
-    public function __construct(HttpKernelInterface $app, array $contentTypes = array())
+    public function __construct(HttpKernelInterface $app, $contentType = 'application/json')
     {
         $this->app = $app;
+        $this->contentType = $contentType;
     }
 
     public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
     {
-        if (in_array($request->headers->get('Content-Type'), $this->contentTypes, true)) {
+        if (0 === strpos($request->headers->get('Content-Type'), $this->contentType)) {
             $data = json_decode($request->getContent(), true);
             $request->request->replace(is_array($data) ? $data : array());
         }
 
         return $this->app->handle($request, $type, $catch);
+    }
+
+    public function terminate(Request $request, Response $response)
+    {
+        $request->headers->set('Content-Type', $this->contentType);
+        $request->headers->set('Accept', $this->contentType);
+
+        if ($this->app instanceof TerminableInterface) {
+            $this->app->terminate($request, $response);
+        }
     }
 }
